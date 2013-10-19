f8c1fc5c63109a3e354a829b0c0c5883c2239272
Tim Behrsin <tim@behrsin.com>
2013-10-19 13:06:35 +0100

    Initial Commit

---
+++ App55Tests/App55Tests.m
@@ -0,0 +1,34 @@
+//
+//  App55Tests.m
+//  App55Tests
+//
+//  Created by Tim Behrsin on 19/10/2013.
+//  Copyright (c) 2013 App55 Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface App55Tests : XCTestCase
+
+@end
+
+@implementation App55Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
